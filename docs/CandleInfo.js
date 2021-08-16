49815717a2adfc05ce31f89c053b550d3904d9d3
pastelmind <keepyourhonor@gmail.com>
2021-08-16 16:46:54 +0900

    Break word at slashes

--- docs/CandleInfo.js
+++ docs/CandleInfo.js
@@ -10,10 +10,13 @@ export const CandleInfo = Vue.extend({
   computed: {
     effectShortHtml() {
       if (!this.candle) return "";
-      return /** @type {CandleData} */ (this.candle).effect.replace(
-        /Hot(?: Spell)? Dmg/gi,
-        (match) => `<span class="element-hot">${match}</span>`
-      );
+
+      return /** @type {CandleData} */ (this.candle).effect
+        .replace(/\//g, "&#8203;/&#8203;") // Allow breaking word at slashes
+        .replace(
+          /Hot(?: Spell)? Dmg/gi,
+          (match) => `<span class="element-hot">${match}</span>`
+        );
     },
   },
   template: "#candle-info-template",
